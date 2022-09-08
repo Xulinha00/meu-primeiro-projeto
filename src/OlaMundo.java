adbf29ac7fb65500bfae8443d4c9ad0e334f84a4
Xulinha00 <juliafonsodesouza@gmail.com>
2022-09-07 21:04:50 -0300

    add switch

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,41 @@
 public class OlaMundo {
+	
 	public static void main(String args[]) {
-		System.out.println("Hello World Xulinha!");
+		
+		int i;
+		
+		System.out.println("..MOTIVOS PARA AMOR O THIRIRI..");
+		
+		for( i = 1; i < 5; i++) {
+			
+			switch(i) {
+			
+			case 1:
+				System.out.println("Pq ele é muitoo fofo");
+			break;
+			
+			case 2:
+				System.out.println("Pq ele é o melhor professor do mundo");
+			break;
+				
+			case 3:
+				System.out.println("Pq ele é muito cheiroso");
+			break;
+				
+			case 4:
+				System.out.println("Pq ele é o melhor namorado do mundo");
+			break;
+				
+			case 5:
+				System.out.println("Pq ele é yag comigo");
+			break;
+			
+			default:
+				System.out.println("Erro");
+			break;
+
+			}
+		}
+		
 	}
 }
